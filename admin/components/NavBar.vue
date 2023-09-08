abf051f92acefca1fc7d7b053a37febb03750918
adamkoro <adam.koronics@gmail.com>
2023-09-08 14:14:06 +0200

    fix: buttons

--- admin/components/NavBar.vue
+++ admin/components/NavBar.vue
@@ -1,27 +1,25 @@
 <template>
     <nav class="mt-1 mx-2 bg-green-900 border-b-2 rounded-lg border-orange-600 text-white dark:text-black">
-        <div class="flex items-center justify-between p-3">
+        <div class="w-full flex items-center justify-between p-3">
             <div class="flex items-center">
                 <img src="~/assets/pictures/geeko.svg" class="h-6 mr-2" />
-                <span class="text-xl font-semibold whitespace-nowrap ">AdventCalendar Admin Center</span>
+                <div class="text-xl font-semibold ">AdventCalendar Admin Center</div>
             </div>
-            <div class=" hidden w-full md:block md:w-auto ">
-                <ul class=" font-medium flex gap-5 items-center">
-                    <div class=" hover:text-orange-600">
-                        <div class="">Welcome, {{ username }}</div>
-                    </div>
-                    <div class="hover:text-orange-600">
-                        <a href="/admin/home">Home</a>
-                    </div>
-                    <div class="hover:text-orange-600">
-                        <a href="/admin/users">Users</a>
-                    </div>
-                    <div class="hover:text-orange-600">
-                        <button class="" @click="logout">Logout</button>
-                    </div>
+            <div class="flex items-center hover:text-orange-600">
+                <div class="flex items-center">Welcome, {{ username }}</div>
+            </div>
+            <div class="hidden w-full md:block md:w-auto ">
+                <ul class="font-medium flex gap-3 items-center">
                     <div class="">
-                        <LightDarkButton/>
+                        <LightDarkButton />
                     </div>
+                    <UButton>
+                        <NuxtLink to="/admin/home">Home</NuxtLink>
+                    </UButton>
+                    <UButton>
+                        <NuxtLink to="/admin/users">Users</NuxtLink>
+                    </UButton>
+                    <UButton @click="logout" label="Logout" />
                 </ul>
             </div>
         </div>
@@ -30,29 +28,29 @@
 <script setup>
 const username = useCookie('username')
 const logout = async () => {
-        const { data, error } = await useFetch(useRuntimeConfig().public.apiUrl+'/api/logout', {
+    const { data, error } = await useFetch(useRuntimeConfig().public.apiUrl + '/api/logout', {
         method: 'POST',
-        headers: useRequestHeaders(['authorization','cookie',]),
+        headers: useRequestHeaders(['authorization', 'cookie',]),
         credentials: 'include',
-        })
-        if (error.value != null){
-            alert(error.value.data.error)
-            return
-        }
-        if (data.value.status == 'Logout successful') {
-            deleteCookie("username")
-            navigateTo('/admin/login',{ redirect: true })
-        }
-        else {
-            alert(data.value.data.error)
-        }
+    })
+    if (error.value != null) {
+        alert(error.value.data.error)
+        return
     }
+    if (data.value.status == 'Logout successful') {
+        deleteCookie("username")
+        navigateTo('/admin/login', { redirect: true })
+    }
+    else {
+        alert(data.value.data.error)
+    }
+}
 const colorMode = useColorMode()
 const isDark = computed({
-    get () {
+    get() {
         return colorMode.value === 'dark'
     },
-    set () {
+    set() {
         colorMode.preference = colorMode.value === 'dark' ? 'light' : 'dark'
     }
 })
