7b1a54f541695c41a96bf602f5420e37b4c295c2
adamkoro <adam.koronics@gmail.com>
2023-09-18 19:41:46 +0200

    feat: middleware

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,4 +1,13 @@
 <script setup>
 definePageMeta({
+    middleware: [
+        function (to, from) {
+            const token = useCookie('token')
+            if (token.value) {
+                navigateTo('/admin/home',{ redirect: true })
+            }
+            navigateTo('/admin/login',{ redirect: true })
+        }
+    ],
 })
 </script>
