b0b9ea4c363c7ac572579d92088ec9bb23b90e1b
adamkoro <adam.koronics@gmail.com>
2023-09-19 07:57:26 +0200

    fix: middleare route

--- admin/middleware/checkAuth.ts
+++ admin/middleware/checkAuth.ts
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
     const cookie = useCookie('token')
-    if (cookie.value == '') {
+    if (cookie.value != '') {
         return navigateTo('/admin/home')
     } else {
         return navigateTo('/admin/login')
