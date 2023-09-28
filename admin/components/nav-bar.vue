4bbe243fa22432510a41bf70fcae060ee0dcc896
adamkoro <adam.koronics@gmail.com>
2023-09-28 18:04:37 +0200

    fix: unneeded link parameter, and fix logout route

--- admin/components/nav-bar.vue
+++ admin/components/nav-bar.vue
@@ -12,45 +12,45 @@
             <ul class="flex items-center">
                 <div v-if="activeNavLink == 'home'">
                     <li class="p-4 border-b-2 border-primary-500 border-opacity-100 text-primary-500 cursor-pointer">
-                        <NuxtLink to="/admin/home" target="_parent">Home</NuxtLink>
+                        <NuxtLink to="/admin/home" >Home</NuxtLink>
                     </li>
                 </div>
                 <div v-else >
                     <li class="p-4 border-b-2 border-primary-500 border-opacity-0 hover:border-opacity-100 hover:text-primary-500 duration-200 cursor-pointer">
-                        <NuxtLink to="/admin/home" target="_parent">Home</NuxtLink>
+                        <NuxtLink to="/admin/home" >Home</NuxtLink>
                     </li>
                 </div>
                 <div v-if="activeNavLink == 'days'">
                     <li class="p-4 border-b-2 border-primary-500 border-opacity-100 text-primary-500 cursor-pointer">
-                        <NuxtLink to="/admin/days" target="_parent">Days</NuxtLink>
+                        <NuxtLink to="/admin/days" >Days</NuxtLink>
                     </li>
                 </div>
                 <div v-else >
                 <li
                     class="p-4 border-b-2 border-primary-500 border-opacity-0 hover:border-opacity-100 hover:text-primary-500 duration-200 cursor-pointer">
-                    <NuxtLink to="/admin/days" target="_parent">Days</NuxtLink>
+                    <NuxtLink to="/admin/days" >Days</NuxtLink>
                 </li>
                 </div>
                 <div v-if="activeNavLink == 'users'">
                     <li class="p-4 border-b-2 border-primary-500 border-opacity-100 text-primary-500 cursor-pointer">
-                        <NuxtLink to="/admin/users" target="_parent">Users</NuxtLink>
+                        <NuxtLink to="/admin/users" >Users</NuxtLink>
                     </li>
                 </div>
                 <div v-else >
                 <li
                     class="p-4 border-b-2 border-primary-500 border-opacity-0 hover:border-opacity-100 hover:text-primary-500 duration-200 cursor-pointer">
-                    <NuxtLink to="/admin/users" target="_parent">Users</NuxtLink>
+                    <NuxtLink to="/admin/users" >Users</NuxtLink>
                 </li>
                 </div>
                 <div v-if="activeNavLink == 'emails'">
                     <li class="p-4 border-b-2 border-primary-500 border-opacity-100 text-primary-500 cursor-pointer">
-                        <NuxtLink to="/admin/emails" target="_parent">Emails</NuxtLink>
+                        <NuxtLink to="/admin/emails" >Emails</NuxtLink>
                     </li>
                 </div>
                 <div v-else >
                 <li
                     class="p-4 border-b-2 border-primary-500 border-opacity-0 hover:border-opacity-100 hover:text-primary-500 duration-200 cursor-pointer">
-                    <NuxtLink to="/admin/emails" target="_parent">Emails</NuxtLink>
+                    <NuxtLink to="/admin/emails" >Emails</NuxtLink>
                 </li>
                 </div>
             </ul>
@@ -82,9 +82,14 @@ const logout = async () => {
         alert(error.value.data.error)
         return
     }
-    if (data.value.status == 'logout successful') {
+    if (data.value.status === 'logout successful') {
         deleteCookie("username")
-        navigateTo('/admin/login', { redirect: true })
+        navigateTo('/admin/login',{
+            replace: true,
+            redirectCode: 302,
+            external: false,
+            open: false,
+        })
     }
     else {
         alert(data.value.data.error)
