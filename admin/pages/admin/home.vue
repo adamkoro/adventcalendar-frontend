88ef55f306ec765be7de95ec729096b2ab81dbe2
adamkoro <adam.koronics@gmail.com>
2023-09-28 18:06:10 +0200

    chore: add minimal info

--- admin/pages/admin/home.vue
+++ admin/pages/admin/home.vue
@@ -1,13 +1,16 @@
 <template>
-    <NavBar/>
-    <div>
-        <div class="w-full p-2 flex justify-between items-center"/>
+    <NavBar />
+    <div class="flex flex-col items-center justify-center">
+        <div class="text-3xl font-semibold">Welcome to the admin panel!</div>
+        <div class="text-xl font-semibold">Please use the navigation bar above to navigate the admin panel.</div>
     </div>
 </template>
 
 <script setup>
 import nuxtStorage from 'nuxt-storage'
+
 nuxtStorage.localStorage.setData('activeNavLink', 'home')
+
 useHead({
     title: `Admin Home`,
     link: [{ hid: 'icon', rel: 'icon', type: 'image/svg', href: '/favicon.svg' }]
