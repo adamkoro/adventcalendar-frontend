e923ca1e7f06a1c0ec8d8d2ba26b653c30478301
adamkoro <adam.koronics@gmail.com>
2023-09-30 18:24:38 +0200

    fix: edit modal append with header info

--- admin/pages/admin/emails.vue
+++ admin/pages/admin/emails.vue
@@ -114,6 +114,8 @@
             <div class="flex items-center justify-between ">
               <h3 class="text-base font-semibold leading-6 text-gray-900 dark:text-white">
                 Edit email pattern
+                |
+                Name: {{ editSelectedEmail.name }}
               </h3>
               <UButton color="gray" variant="ghost" icon="i-heroicons-x-mark-20-solid" class="-my-1"
                 @click="(isEditOpen = false)" />
